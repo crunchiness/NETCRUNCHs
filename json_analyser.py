2122e8860e7b50bcebc074d82dac696639a5c4c2
Ingvaras Merkys <ingvaras@gmail.com>
2015-03-24 17:01:36 +0000

    json analyser comments and stuff

--- json_analyser.py
+++ json_analyser.py
@@ -1,26 +1,22 @@
-# -*- coding: utf-8 -*-
-__author__ = 'crunch'
+#!/usr/bin/env python
+__author__ = 'Ingvaras Merkys'
+
 import json
-import sys
 import time
+import argparse
 from timestamp import TimeStamp
 
 
 def write_stats(stats):
-    # stats = {
-    #     'websites': {},
-    #     'from_cache': 0,
-    #     'total': 0,
-    #     'response_only': 0,
-    #     'request_only': 0,
-    #     'no_data': 0,
-    #     'wtf': {}
-    # }
     stats_file = open('stats.log', 'a')
     stats_file.write('='*100 + '\n')
+    stats_file.write('json_analyser.py\n')
     stats_file.write(time.strftime("%c") + '\n')
     stats_file.write('='*100 + '\n')
-    stats_file.write(str(stats['websites']) + '\n')
+    stats_file.write('Web app\t# of pairs\n')
+    for key in stats['websites']:
+        stats_file.write(key + '\t' + str(stats['websites'][key]) + '\n')
+    stats_file.write('='*100 + '\n')
     stats_file.write('# of pairs (incl. empty): {0}\n'.format(stats['total']))
     stats_file.write('# of empty pairs: {0}\n'.format(stats['no_data']))
     stats_file.write('# of pairs with request only: {0}\n'.format(stats['request_only']))
@@ -31,7 +27,7 @@ def write_stats(stats):
 
 
 def preprocess_json(data):
-    """Filter out cache responses, build statistics"""
+    """Filter out cache responses, bad data, build statistics"""
     pairs = data['pairs']
     stats = {
         'websites': {},
@@ -48,24 +44,27 @@ def preprocess_json(data):
         for j, pair in enumerate(pairs[website]['pairs']):
             if pair is None:
                 stats['no_data'] += 1
-                try:
-                    stats['wtf'][website].append(j)
-                except KeyError:
-                    stats['wtf'][website] = [j]
+            # request and response are missing
             elif pair['response'] is None and pair['request'] is None:
                 stats['no_data'] += 1
+            # only response if missing
             elif pair['response'] is None:
                 stats['request_only'] += 1
+            # only request is missing
             elif pair['request'] is None:
                 stats['response_only'] += 1
+            # everything is ok
             else:
+                # record and dismiss if from cache
                 if pair['response']['fromCache']:
                     stats['from_cache'] += 1
                 else:
+                    # save good pair
                     try:
                         filtered_data[website].append(pair)
                     except KeyError:
                         filtered_data[website] = [pair]
+            # count all pairs
             stats['websites'][website] += 1
             stats['total'] += 1
     write_stats(stats)
@@ -73,6 +72,7 @@ def preprocess_json(data):
 
 
 def preprocess_time_tab_data(json_data):
+    """Transforms active tab data into more usable form"""
     active_tab = json_data['activeTab']
     tab_changes = json_data['tabChanges']
     opened_website = []
@@ -103,6 +103,7 @@ def preprocess_time_tab_data(json_data):
                     'website': website
                 })
                 break
+        # if didn't break
         else:
             opened_website.append({
                 'timeStamp': time_stamp_from,
@@ -112,6 +113,7 @@ def preprocess_time_tab_data(json_data):
 
 
 def get_avg_delays(data):
+    # calculates average delay between request and response
     delays = {}
     for website in data:
         diff_sum = 0
@@ -125,6 +127,7 @@ def get_avg_delays(data):
 
 
 def get_json_time_limits(data):
+    """"Gets accurate time limits of JSON data"""
     first_request_time = float('inf')
     last_response_time = 0
     for website in data:
@@ -135,20 +138,28 @@ def get_json_time_limits(data):
                 last_response_time = pair['response']['timeStamp']
     return TimeStamp(first_request_time / 1000.0), TimeStamp(last_response_time / 1000.0)
 
-if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        filename = 'data.json'
-    else:
-        filename = sys.argv[1]
 
-    f = open(filename)
-    data = json.load(f)
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='JSON analyser.')
+    parser.add_argument('json_name', nargs='?', type=str, help='input JSON file (\'data.json\' by default)',
+                        default='data.json')
+    args = parser.parse_args()
+    file_name = args.json_name
+    input_file = open(file_name)
+    data = json.load(input_file)
+
+    # filter data
     filtered_pairs = preprocess_json(data)
+
+    # preprocess active tab data
     prepared_time = preprocess_time_tab_data(data)
-    a, b = get_json_time_limits(filtered_pairs)
-    new_filename = filename[:-5] + '_preprocessed.json'
-    f1 = open(new_filename, 'w')
+
+    # write preprocessed data into new file
+    file_name_out = file_name[:-5] + '_preprocessed.json'
+    output_file = open(file_name_out, 'w')
     json.dump({
         'pairs': filtered_pairs,
         'timing': prepared_time
-    }, f1)
+    }, output_file)
+    output_file.close()
