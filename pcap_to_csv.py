7baf3eec512441496238cad9f304e5eedf9d975b
Ingvaras Merkys <ingvaras@gmail.com>
2016-03-06 18:54:44 +0000

    updated pcap->csv conversion script

--- pcap_to_csv.py
+++ pcap_to_csv.py
@@ -3,10 +3,10 @@ import binascii
 from pcapfile import savefile
 from pcapfile.protocols.network.ip import IP
 from pcapfile.protocols.linklayer.ethernet import Ethernet
-from pcap_analyser import parse, is_ack, get_ports
+from pcap_analyser import parse, is_ack, get_ports, is_rst, is_fin
 
 FIELD_NAMES = ['timestamp', 'v', 'hl', 'tos', 'len', 'id', 'flags', 'off', 'ttl', 'protocol', 'sum', 'src', 'dst',
-               'opt', 'pad', 'website', 'source', 'is_ack', 'url', 'src_port', 'dst_port']
+               'opt', 'pad', 'website', 'source', 'is_ack', 'is_rst', 'is_fin', 'url', 'src_port', 'dst_port']
 
 
 def parse_pcap(packets, csv_name='android_el_manana.csv'):
@@ -24,6 +24,8 @@ def parse_pcap(packets, csv_name='android_el_manana.csv'):
             continue
         pkt_obj = parse(ip_packet)
         pkt_obj['is_ack'] = is_ack(pkt_obj, ip_packet)
+        pkt_obj['is_rst'] = is_rst(pkt_obj, ip_packet)
+        pkt_obj['is_fin'] = is_fin(pkt_obj, ip_packet)
         pkt_obj['timestamp'] = pkt_ts
         pkt_obj['src_port'], pkt_obj['dst_port'] = get_ports(pkt_obj, ip_packet)
         pkt_obj['source'] = ''
